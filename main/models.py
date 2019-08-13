e4d557cdcae88a1bc1b7061f5096f12e747027e3
friederjanmoerman <hijbrid@gmail.com>
2019-08-13 14:47:46 +0200

    trying to get custom css working

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField("date published", default=datetime.now())
+    tutorial_published = models.DateTimeField("date published", default=datetime.now)
 
     def __str__(self):
         return self.tutorial_title
